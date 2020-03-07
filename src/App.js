eb48d12355376231214ae95f7fa70bd69020eeac
rasha <rasha.abdulrazzak@gmail.com>
2020-03-07 07:52:10 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import Clock from './Clock'
 
 class App extends React.Component {
 constructor() {
   super()
   this.state = {
-    clockIDs: [1,2]
-  }
+    clockIDs: [1,2]  }
+}
+handleAdd =()=>{
+  let array = this.state.clockIDs
+  array.push(array.length+1)
+  this.setState({clockIDs: array})
 }
   render() {
     return (
-      <div></div>
+      <div>       
+          {this.state.clockIDs.map(id=><Clock key={id} />)}
+          <button onClick={this.handleAdd}>Add New clock </button>          
+      </div> 
+     
     );
   
   }
